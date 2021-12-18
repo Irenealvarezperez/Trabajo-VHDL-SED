ac3deeb64477997d382ada831d72b61eec896c30
rgfranco2 <r.gfranco@alumnos.upm.es>
2021-12-18 19:45:00 +0100

    Update decodificador.vhd
    Introducción palabras

--- decodificador.vhd
+++ decodificador.vhd
@@ -3,23 +3,81 @@ USE ieee.std_logic_1164.ALL;
 USE ieee.std_logic_arith.ALL;
 USE ieee.std_logic_unsigned.ALL;
 ENTITY decodificador IS
+    generic (
+        long_opcion:positive:=3
+    );
     PORT (
-        seleccion : IN std_logic_vector(3 DOWNTO 0);
-        salida : OUT std_logic_vector(6 DOWNTO 0)
+        seleccion : IN std_logic_vector(long_opcion -1 DOWNTO 0);
+        salida_disp0 : OUT std_logic_vector(6 DOWNTO 0);
+        salida_disp1: OUT std_logic_vector(6 DOWNTO 0);
+        salida_disp2 : OUT std_logic_vector(6 DOWNTO 0);
+        salida_disp3 : OUT std_logic_vector(6 DOWNTO 0);
+        salida_disp4 : OUT std_logic_vector(6 DOWNTO 0);
+        salida_disp5 : OUT std_logic_vector(6 DOWNTO 0);
+        salida_disp6 : OUT std_logic_vector(6 DOWNTO 0);
+        salida_disp7 : OUT std_logic_vector(6 DOWNTO 0)
     );
 END ENTITY decodificador;
-ARCHITECTURE dataflow OF decodificador IS
+ARCHITECTURE behavioral OF decodificador IS
 BEGIN
-    WITH seleccion SELECT
- salida <= "0000001" WHEN "0000",
-        "1001111" WHEN "0001",
-        "0010010" WHEN "0010",
-        "0000110" WHEN "0011",
-        "1001100" WHEN "0100",
-        "0100100" WHEN "0101",
-        "0100000" WHEN "0110",
-        "0001111" WHEN "0111",
-        "0000000" WHEN "1000",
-        "0000100" WHEN "1001",
-        "1111110" WHEN others;
-END ARCHITECTURE dataflow;
+    process(seleccion)
+    begin
+        case seleccion is
+
+            when 1=> --Nivel azúcar> AZUCAR
+                salida_disp7<="0001000";--A
+                salida_disp6<="0010010";--Z
+                salida_disp5<="100001";--U
+                salida_disp4<="0110001";--C
+                salida_disp3<="0001000";--A
+                salida_disp2<="0001000";--R
+                salida_disp1<="1111111";--vacio
+                salida_disp0<="1111111";--vacio
+            when 2=> --corto
+                salida_disp7<="1001111";--1
+                salida_disp6<="0000001";--0
+                salida_disp5<="0100100";--S
+                salida_disp4<="0110000";--E
+                salida_disp3<="010000";--G
+                salida_disp2<="1111111";--vacio
+                salida_disp1<="1111111";--vacio
+                salida_disp0<="1111111";--vacio 
+            when 3=> --largo
+                salida_disp7<="0010010";--2
+                salida_disp6<="0000001";--0
+                salida_disp5<="0100100";--S
+                salida_disp4<="0110000";--E
+                salida_disp3<="010000";--G
+                salida_disp2<="1111111";--vacio
+                salida_disp1<="1111111";--vacio
+                salida_disp0<="1111111";--vacio 
+            when 4=> -- con leche
+                salida_disp7<="0001110";--L
+                salida_disp6<="0110000";--E
+                salida_disp5<="0110001";--C
+                salida_disp4<="1001000";--H
+                salida_disp3<="0110000";--E
+                salida_disp2<="1111111";--vacio
+                salida_disp1<="1111111";--vacio
+                salida_disp0<="1111111";--vacio   
+            when 5=> -- sin leche
+                salida_disp7<="0001001";--N
+                salida_disp6<="0000001";--O
+                salida_disp5<="1111111";--vacio 
+                salida_disp4<="0001110";--L
+                salida_disp3<="0110000";--E
+                salida_disp2<="0110001";--C
+                salida_disp1<="1001000";--H
+                salida_disp0<="0110000";--E
+            when others=>
+                salida_disp7<="1111111";--vacio
+                salida_disp6<="1111111";--vacio
+                salida_disp5<="1111111";--vacio 
+                salida_disp4<="1111111";--vacio
+                salida_disp3<="1111111";--vacio
+                salida_disp2<="1111111";--vacio
+                salida_disp1<="1111111";--vacio
+                salida_disp0<="1111111";--vacio
+        end case;
+    end process;
+end behavioral;
