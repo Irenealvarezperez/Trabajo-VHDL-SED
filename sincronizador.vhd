adc22ff3bfcf355686b74d0518cf5330e4e8c2d0
rgfranco2 <r.gfranco@alumnos.upm.es>
2022-01-16 12:52:30 +0100

    Versión final comentada

--- sincronizador.vhd
+++ sincronizador.vhd
@@ -1,41 +1,12 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 08.12.2021 19:58:02
--- Design Name: 
--- Module Name: sincronizador - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity sincronizador is
     PORT (
         CLK : in std_logic;
-        SYNC_IN : in std_logic;
-        SYNC_OUT : out std_logic
+        SYNC_IN : in std_logic;--Entrada sincronizador
+        SYNC_OUT : out std_logic--Salida sincronizador
     );
 end sincronizador;
 
